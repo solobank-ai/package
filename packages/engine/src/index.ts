ba3a75745f6a65ae5423d3c1de92adb7d7b0ac76
Magurin <stolarovigor734@gmail.com>
2026-04-07 03:04:02 +0500

    revert: remove @solobank/engine package

--- packages/engine/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { QueryEngine } from './engine.js';
-export type { EngineEvent } from './engine.js';
-export { OpenAIProvider } from './provider.js';
-export type { ChatMessage, ProviderUsage } from './provider.js';
-export { SessionStore } from './session.js';
-export type { Session } from './session.js';
-export { CostTracker } from './cost.js';
-export { TOOL_DEFINITIONS, READ_TOOLS, executeTool } from './tools.js';
-export type { Agent, ToolDefinition } from './tools.js';
